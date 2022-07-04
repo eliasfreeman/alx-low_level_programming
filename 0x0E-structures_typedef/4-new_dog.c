4d54561c7812eeb5fe3bd7ed1ce72ae7085bf948
eliasfreeman <oladimejiboluwaji@gmail.com>
2022-07-04 16:45:16 +0100

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,84 +1,53 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 #include <stdio.h>
-
 /**
- * _copy  -   Make a copy of passed in argument
- * @src:      Data to make copy of
- * Return:    Pointer
- */
-
-char *_copy(char *src)
-{
-	char *ptr;
-	int x, len;
-
-	if (src == NULL)
-	{
-		return (NULL);
-	}
-
-	for (len = 0; src[len] != '\0'; len++)
-		;
-
-	ptr = malloc(sizeof(char) * (len + 1));
-
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-
-	for (x = 0; src[x] != '\0'; x++)
-	{
-		ptr[x] = src[x];
-	}
-
-	ptr[x] = '\0';
-	return (ptr);
-}
-
-/**
- * new_dog     - Create a new dog variable
- * @name:        Name of the dog
- * @age:         Age of the dog
- * @owner:       Owner of the dog
- * Return:       Pointer to new dog variable
- */
-
+*new_dog - struct returning
+*@name: dog name
+*@age: age of dog
+*@owner: owner name
+*Return: dog struct
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *bingo;
-	char *new_name, *new_owner;
+	dog_t *dog1;
+	int nameSize = 0, ownerSize = 0, loop;
 
-	if (name == NULL || owner == NULL)
-	{
-		return (NULL);
-	}
-
-	bing = malloc(sizeof(dog_t));
-	if (bingo == NULL)
-	{
-		return (NULL);
-	}
-
-	new_name = _copy(name);
-	if (new_name == NULL)
-	{
-		free(bingo);
-		return (NULL);
-	}
-	(*bingo).name = new_name;
-
-	(*bingo).age = age;
-
-	new_owner = _copy(owner);
-	if (new_owner == NULL)
-	{
-		free((*bingo).name);
-		free(bingo);
-		return (NULL);
-	}
-	(*bingo).owner = new_owner;
-
-	return (bingo);
+while (name[nameSize] != '\0')
+{
+nameSize++;
+}
+while (owner[ownerSize] != '\0')
+{
+ownerSize++;
+}
+dog1 = malloc(sizeof(dog_t));
+if (dog1 == NULL)
+{
+free(dog1);
+return (NULL);
+}
+dog1->name = malloc(sizeof(dog1->name) * nameSize);
+if (dog1->name == NULL)
+{
+free(dog1->name);
+free(dog1);
+return (NULL);
+}
+for (loop = 0; loop <= nameSize; loop++)
+{
+dog1->name[loop] = name[loop];
+}
+dog1->age = age;
+dog1->owner = malloc(sizeof(dog1->owner) * ownerSize);
+if (dog1->owner == NULL)
+{
+free(dog1->owner);
+free(dog1->name);
+free(dog1);
+return (NULL);
+}
+for (loop = 0; loop <= ownerSize; loop++)
+dog1->owner[loop] = owner[loop];
+return (dog1);
 }
